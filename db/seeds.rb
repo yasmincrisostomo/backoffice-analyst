8ec35467c371a91c6c792940b849a52bb21b1fa2
Yasmin Crisostomo <crisostomyasmin@gmail.com>
2023-03-25 18:43:06 -0300

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'csv'
+
+CSV.foreach('db/csv/clientes.csv', headers: true) do |row|
+  Cliente.create(user_id: row['user_id'], cnpj: row['cnpj'], nome_do_cliente: row['nome_do_cliente'])
+end
+
+CSV.foreach('db/csv/statuses.csv', headers: true) do |row|
+  Status.create(user_id: row['user_id'], status: row['status'], data_horario_do_status: row['data_horario_do_status'])
+end
