fd005f179fd4079ef1acc5898d9638515d8a445a
Yasmin Crisostomo <crisostomyasmin@gmail.com>
2023-03-25 18:43:12 -0300

    associations

--- app/models/cliente.rb
+++ app/models/cliente.rb
@@ -1,2 +1,3 @@
 class Cliente < ApplicationRecord
+  has_many :status, foreign_key: 'user_id'
 end
--- app/models/status.rb
+++ app/models/status.rb
@@ -1,2 +1,4 @@
 class Status < ApplicationRecord
+  belongs_to :cliente, foreign_key: 'user_id'
+  enum status: [:pending_kyc, :pending_purchase, :manual_check, :approved, :blocked, :inactive]
 end
