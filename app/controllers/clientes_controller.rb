31e99e8fbc2ad178db45758fdba1a329dd64f1b7
Yasmin Crisostomo <crisostomyasmin@gmail.com>
2023-03-28 13:35:36 -0300

    fix index method

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -3,28 +3,27 @@ class ClientesController < ApplicationController
     @query = params[:cnpj]
 
     @results = Cliente.select("clientes.cnpj, statuses_pending.data_horario_do_status as date_of_purchase,
-                              (strftime('%s', statuses_approved.data_horario_do_status) - strftime('%s', statuses_pending.data_horario_do_status))/3600 as approval_time_hours,
-                              ((strftime('%s', statuses_approved.data_horario_do_status) - strftime('%s', statuses_pending.data_horario_do_status)) % 3600)/60 as approval_time_minutes")
+                            (strftime('%s', statuses_approved.data_horario_do_status) - strftime('%s', statuses_pending.data_horario_do_status))/3600 as approval_time_hours,
+                            ((strftime('%s', statuses_approved.data_horario_do_status) - strftime('%s', statuses_pending.data_horario_do_status)) % 3600)/60 as approval_time_minutes")
                       .joins("JOIN statuses statuses_pending ON clientes.user_id = statuses_pending.user_id AND statuses_pending.status = 'pending_kyc' 
-                                JOIN statuses statuses_approved ON clientes.user_id = statuses_approved.user_id AND statuses_approved.status = 'approved'")
+                              JOIN (SELECT user_id, MAX(data_horario_do_status) as max_data_horario_do_status FROM statuses WHERE status = 'approved' GROUP BY user_id) last_approved
+                              ON clientes.user_id = last_approved.user_id
+                              JOIN statuses statuses_approved ON clientes.user_id = statuses_approved.user_id AND statuses_approved.status = 'approved' AND statuses_approved.data_horario_do_status = last_approved.max_data_horario_do_status")
                       .where(where_clause)
                       .order('statuses_pending.data_horario_do_status DESC')
                       .page(params[:page])
 
     @avg_time = Cliente.joins(:statuses)
-                       .where(where_clause)
                        .where("statuses.status = 'pending_kyc' OR statuses.status = 'approved'")
                        .group(:user_id)
                        .average("(strftime('%s', statuses.data_horario_do_status))")
 
     @max_time = Cliente.joins(:statuses)
-                       .where(where_clause)
                        .where("statuses.status = 'pending_kyc' OR statuses.status = 'approved'")
                        .group(:user_id)
                        .maximum("(strftime('%s', statuses.data_horario_do_status))")
 
     @min_time = Cliente.joins(:statuses)
-                       .where(where_clause)
                        .where("statuses.status = 'pending_kyc' OR statuses.status = 'approved'")
                        .group(:user_id)
                        .minimum("(strftime('%s', statuses.data_horario_do_status))")
@@ -35,8 +34,20 @@ class ClientesController < ApplicationController
   private
 
   def where_clause
-    return "clientes.cnpj LIKE '%#{@query}%'" if @query
-
-    '1=1'
+    where = "1=1"
+    where += " AND clientes.cnpj LIKE '%#{@query}%'" if @query
+    where += " AND statuses_pending.id IN (
+                    SELECT MAX(id)
+                    FROM statuses
+                    WHERE status = 'pending_kyc'
+                    GROUP BY user_id
+                )"
+    where += " AND statuses_approved.id IN (
+                    SELECT MAX(id)
+                    FROM statuses
+                    WHERE status = 'approved'
+                    GROUP BY user_id
+                )"
+    where
   end
 end
