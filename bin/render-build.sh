93363ec5a2656afedfe970de4cd6c76ded8f2e55
Yasmin Crisostomo <crisostomyasmin@gmail.com>
2023-03-29 00:45:55 -0300

    seed

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,4 +5,7 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+bundle exec rails db:drop
+bundle exec rails db:create
 bundle exec rails db:migrate
+bundle exec rails db:seed
